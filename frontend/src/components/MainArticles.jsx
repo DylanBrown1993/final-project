6984ad41a32a595096f756460a9228ba27ad0341
= <=>
2023-07-13 10:02:18 -0400

    refactoring front end code for styling

--- frontend/src/components/MainArticles.jsx
+++ frontend/src/components/MainArticles.jsx
@@ -20,25 +20,23 @@ const MainArticle = () => {
   }, []);
 
   return (
-    <div className="main-article-route">
-      <div className="main-articles-container">
-        <h2>Articles</h2>
-        <div className="main-article-grid">
-          {articles.map((article, index) => (
-            <div key={article.id} className={`main-article-item ${index === 0 ? 'main-full-width' : 'home-grid-item'}`}>
-              <Link to={`/article/${article.id}`} className='article-link'>
-                <div className={`main-article-content ${index === 0 ? 'main-article-first' : ''}`}>
-                  <div className="main-image-container">
-                    <img src={article.header_image} alt="" className="article-image" />
-                  </div>
-                  <h2 className="article-title">{article.title}</h2>
-                </div>
-              </Link>
+    <div className="home-articles-container">
+    <h2>Articles</h2>
+    <div className="home-article-grid">
+      {articles.map((article, index) => (
+        <div key={article.id} className={`home-article-item ${index === 0 ? 'home-full-width' : 'home-grid-item'}`}>
+          <Link to={`/article/${article.id}`} className='article-link'>
+            <div className={`home-article-content ${index === 0 ? 'home-article-first' : ''}`}>
+              <div className="article-image-container">
+                <img src={article.header_image} alt="" className="article-image" />
+              </div>
+              <h2 className="article-title">{article.title}</h2>
             </div>
-          ))}
+          </Link>
         </div>
-      </div>
-      </div>
+      ))}
+    </div>
+  </div>
   );
 };
 export default MainArticle;
