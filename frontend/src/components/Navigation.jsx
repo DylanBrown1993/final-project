b0e263bc08f64c2b510dffbfc061f63e061f2504
= <=>
2023-07-13 18:31:04 -0400

    updated buttons and continued styling

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useRef } from 'react';
 import '../styles/Navigation.css';
 import logoImage from '../../../src/images/TitleNav.png';
+import ButtonImage from '../../../src/images/LilyButton.png';
 import { Link, NavLink } from 'react-router-dom';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
@@ -30,19 +31,25 @@ function Navigation(props) {
         <img src={logoImage} alt="Ribbit" className="logo" />
       </Link>
       <nav className="navbar-links" ref={navRef}>
-        <NavLink exact to="/articles" activeClassName="active">
+        <NavLink
+          exact
+          to="/articles"
+          activeClassName="active"
+          className="nav-link"
+        >
           Articles
         </NavLink>
-        <NavLink to="/reviews" activeClassName="active">
+        <NavLink to="/reviews" activeClassName="active" className="nav-link">
           Reviews
         </NavLink>
-        <NavLink to="/games" activeClassName="active">
+        <NavLink to="/games" activeClassName="active" className="nav-link">
           Games
         </NavLink>
-        <NavLink to="/art" activeClassName="active">
+        <NavLink to="/art" activeClassName="active" className="nav-link">
           Art
         </NavLink>
       </nav>
+
 
       {props.user ? (
         <div className='user-text'>
